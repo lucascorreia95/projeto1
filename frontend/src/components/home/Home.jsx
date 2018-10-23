1ede2ff0991defa7162f37b197f9cf4037386e91
Lucas Correia <lucascorreia-.-@live.com>
2018-10-23 19:17:32 -0300

    primeiro commit

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props =>
+    <Main icon="home" title="Inicio"
+        subtitle="Segundo Projeto React.">
+        <div className="display-4">
+            Bem-Vindo!
+        </div>
+        <hr />
+        <p className="mb-0">
+            Sistema para exemplificar a construcao de um cadastro
+            desenvolvido em React
+        </p>
+    </Main>
