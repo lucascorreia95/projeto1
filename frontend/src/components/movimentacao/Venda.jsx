60d47d756aa93e477f27dea297b3f655e7dd0af2
Lucas Correia <lucascorreia-.-@live.com>
2018-10-29 20:29:34 -0300

    mudancas na venda

--- frontend/src/components/movimentacao/Venda.jsx
+++ frontend/src/components/movimentacao/Venda.jsx
@@ -13,7 +13,10 @@ const initialState = {
     listaProdutos: [],
     produtos: [],
     cliente: '',
-    produto: ''
+    idCliente:'',
+    produto: '',
+    idProduto:'',
+    vendas:[]
 }
 
 const baseUrlClientes = 'http://localhost:3001/users'
@@ -25,54 +28,58 @@ export default class Venda extends Component {
     state = { ...initialState}
 
     componentWillMount(){
-        this.carregaEstado()
-    }
-
-    carregaEstado(){
-        axios(baseUrlClientes).then(resp => {
-            this.setState({ listaClientes: resp.data })
-        })
-        axios(baseUrlProdutos).then(resp => {
-            this.setState({ listaProdutos: resp.data })
+        axios(baseUrlVendas).then(resp => {
+            this.setState({ vendas: resp.data })
         })
     }
 
-    renderClientes(){
-        return this.state.listaClientes.map(cliente => {
-            return(
-                <option key={cliente.id}
-                    value={cliente.name}>{cliente.name}</option>
-            )
+    updatenField(event){
+        const cliente = event.target.value
+        this.setState({ ...this.state, cliente })
+    }
+
+    updatenFieldP(event){
+        const produto = event.target.value
+        this.setState({ ...this.state, produto })
+    }
+
+    buscar(){
+        axios(`${baseUrlClientes}?name_like=${this.state.cliente}`).then(resp => {
+            this.setState({ ...this.state, listaClientes: resp.data, idCliente:'' })
         })
     }
 
-    renderProdutos(){
-        return this.state.listaProdutos.map(produto => {
-            return(
-                <option key={produto.id}
-                    value={produto.name}>{produto.name}</option>
-            )
+    buscarP(){
+        axios(`${baseUrlProdutos}?name_like=${this.state.produto}`).then(resp => {
+            this.setState({ ...this.state, listaProdutos: resp.data })
         })
-    }
-
-    updateState(e){
-        this.setState({...this.state, cliente: e.target.value})
-    }
-
-    updateStateP(e){
-        this.setState({...this.state, produto: e.target.value})
-    }
-
-    addProduto(){
-        let produtos = this.state.produtos
-        produtos.push({name:this.state.produto})
-        this.setState({...this.state, produtos})
         console.log(this.state)
     }
 
+    load(cliente) {
+        const clienteName = cliente.name
+        const clienteId = cliente.id
+        this.setState({ cliente: clienteName, idCliente: clienteId, listaClientes:[] })
+    }
+
+    loadP(produto) {
+        const produtoName = produto.name
+        const produtoId = produto.id
+        this.setState({ produto: produtoName, idProduto: produtoId, listaProdutos:[] })
+    }
+
+    add() {
+        let produtos = this.state.produtos
+        produtos.push({name:this.state.produto, id: this.state.idProduto})
+        this.setState({...this.state, produtos, produto:'', idProduto:''})
+    }
+
+
     save(){
+        console.log(this.state)
         const venda = {
             cliente: this.state.cliente,
+            idcliente: this.state.idCliente,
             produtos: this.state.produtos
         }
         axios.post(baseUrlVendas, venda)
@@ -89,6 +96,78 @@ export default class Venda extends Component {
         //     })
     }
 
+    renderTableClientes() {
+        return(
+            <div className="row">
+                <table className="table mt-4">
+                    <thead>
+                        <tr>
+                            <th>Nome</th>
+                            <th>E-mail</th>
+                            <th>Escolher</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.renderRowsClientes()}
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+
+    renderRowsClientes(){
+        return this.state.listaClientes.map(cliente => {
+            return(
+                <tr key={cliente.id}>
+                    <td>{cliente.name}</td>
+                    <td>{cliente.email}</td>
+                    <td>
+                        <button className="btn btn-primary"
+                            onClick={() => this.load(cliente)}>
+                            <i className="fa fa-check"></i>
+                        </button>
+                    </td>
+                </tr>
+            )
+        })
+    }
+
+    renderTableProdutos() {
+        return(
+            <div className="row">
+                <table className="table mt-4">
+                    <thead>
+                        <tr>
+                            <th>Nome</th>
+                            <th>Marca</th>
+                            <th>Escolher</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.renderRowsProdutos()}
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+
+    renderRowsProdutos(){
+        return this.state.listaProdutos.map(produto => {
+            return(
+                <tr key={produto.id}>
+                    <td>{produto.name}</td>
+                    <td>{produto.brand}</td>
+                    <td>
+                        <button className="btn btn-primary"
+                            onClick={() => this.loadP(produto)}>
+                            <i className="fa fa-check"></i>
+                        </button>
+                    </td>
+                </tr>
+            )
+        })
+    }
+
     renderForm() {
         return (
             <div className="from">
@@ -96,34 +175,38 @@ export default class Venda extends Component {
                     <div className="col-12 col-md-6">
                         <div className="form-group">
                             <label>Cliente:</label>
-                            <select className="form-control"
-                                onChange={e => this.updateState(e)}>
-                                <option key="0"
-                                    value="Selecione">Selecione</option>
-                                {this.renderClientes()}
-                            </select>
+                            <input type="text" className="form-control"
+                                value={this.state.cliente}
+                                onChange={e => this.updatenField(e)}
+                                placeholder="Digite o nome ... " />
+                            <button className="btn btn-primary"
+                                onClick={() => this.buscar()}>
+                                <i className="fa fa-search"></i>
+                            </button>
                         </div>
                     </div>
                 </div>
+                {this.renderTableClientes()}
                 <div className="row">
                     <div className="col-12 col-md-6">
                         <div className="form-group">
                             <label>Produto:</label>
-                            <select className="form-control" id="9"
-                                onChange={e => this.updateStateP(e)}>
-                                <option key="0"
-                                    value="Selecione">Selecione</option>
-                                {this.renderProdutos()}
-                            </select>
+                            <input type="text" className="form-control"
+                                value={this.state.produto}
+                                onChange={e => this.updatenFieldP(e)}
+                                placeholder="Digite o produto ... " />
+                            <button className="btn btn-primary"
+                                onClick={e => this.buscarP(e)}>
+                                <i className="fa fa-search"></i>
+                            </button>
+                            <button className="btn btn-primary"
+                                onClick={e => this.add(e)}>
+                                <i className="fa fa-plus"></i>
+                            </button>
                         </div>
                     </div>
-                    <div className="col-12 col-md-6 d-flex justify-content-end">
-                        <button className="btn btn-primary"
-                            onClick={() => this.addProduto()}>
-                            <i className="fa fa-plus" /> Produto
-                        </button>
-                    </div>
                 </div>
+                {this.renderTableProdutos()}
             </div>
         )
     }
@@ -154,8 +237,47 @@ export default class Venda extends Component {
     renderRows(){
         return this.state.produtos.map(produto => {
             return(
-                <tr key={produto.name}>
+                <tr key={produto.id}>
                     <td>{produto.name}</td>
+                </tr>
+            )
+        })
+    }
+
+    renderTableVendas() {
+        return(
+            <div className="row">
+                <h4 className="mb-3">Vendas Realizadas</h4>
+                <table className="table mt-4">
+                    <thead>
+                        <tr>
+                            <th>Venda</th>
+                            <th>Cliente</th>
+                            <th>Acoes</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.renderRowsVendas()}
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+
+    renderRowsVendas(){
+        return this.state.vendas.map(venda => {
+            return(
+                <tr key={venda.cliente}>
+                    <td>{venda.id}</td>
+                    <td>{venda.cliente}</td>
+                    <td>
+                        <button className="btn btn-warning">
+                            <i className="fa fa-pencil"></i>
+                        </button>
+                        <button className="btn btn-danger ml-2">
+                            <i className="fa fa-trash"></i>
+                        </button>
+                    </td>
                 </tr>
             )
         })
@@ -166,6 +288,7 @@ export default class Venda extends Component {
             <Main {...headerProps}>
                 {this.renderForm()}
                 {this.renderTable()}
+                {this.renderTableVendas()}
             </Main>
         )
     }
